8228542efe65ccc5b216be156ed9c3db89584d51
CaoJiayuan <120106578@qq.com>
2022-08-24 17:03:23 +0800

    update

--- source.go
+++ source.go
@@ -1,9 +1,55 @@
 package datapipe
 
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+const (
+	DefaultPageSize = 20
+)
+
 type Params map[string]interface{}
+type Scopes []func(*gorm.DB) *gorm.DB
+
+func (p Params) WithPage(page, perPage int) Params {
+	p["page"] = PageParam{
+		Page:    page,
+		PerPage: perPage,
+	}
+
+	return p
+}
+
+func (p Params) WithScopes(scopes ...func(db *gorm.DB) *gorm.DB) Params {
+	if _, ok := p["scopes"]; !ok {
+		p["scopes"] = make(Scopes, 0)
+	}
+	if sc, ok := p["scopes"].(Scopes); ok {
+		sc = append(sc, scopes...)
+		p["scopes"] = sc
+	}
+
+	return p
+}
+
+type PageParam struct {
+	Page, PerPage int
+}
+
+type PageMeta struct {
+	Page    int `json:"page"`
+	PerPage int `json:"pre_page"`
+	Total   int `json:"total"`
+}
 
 type DataList[T any] interface {
 	GetData() []T
+}
+
+type PagenationDataList interface {
+	GetPageMeta() PageMeta
 }
 
 type DataSource[T any, K comparable] interface {
@@ -11,4 +57,124 @@ type DataSource[T any, K comparable] interface {
 	GetItem(key K) (T, error)
 	Store(item T) error
 	Delete(key K) error
+}
+
+type SimpleDataList[T any] struct {
+	Items []T      `json:"items"`
+	Meta  PageMeta `json:"meta"`
+}
+
+func (sd *SimpleDataList[T]) GetData() []T {
+	return sd.Items
+}
+
+func NewSimpleDataList[T any](items []T) *SimpleDataList[T] {
+	l := len(items)
+	return NewSimpleDataPageList(items, PageMeta{
+		Page:    1,
+		PerPage: l,
+		Total:   l,
+	})
+}
+
+func NewSimpleDataPageList[T any](items []T, meta PageMeta) *SimpleDataList[T] {
+	return &SimpleDataList[T]{Items: items, Meta: meta}
+}
+
+type DBSource[T any, K comparable] struct {
+	db *gorm.DB
+	pk string
+}
+
+func (ds *DBSource[T, K]) GetList(params Params) DataList[T] {
+	items := make([]T, 0)
+	tx := ds.db.Session(&gorm.Session{})
+
+	if scs, ok := params["scopes"].(Scopes); ok {
+		tx = tx.Scopes(scs...)
+	}
+
+	if page, exists := params["page"]; exists {
+		if pr, ok := page.(PageParam); ok {
+			meta, _ := ds.Pagination(tx, pr.Page, pr.PerPage, &items)
+			return NewSimpleDataPageList(items, meta)
+		}
+	}
+
+	tx.Find(&items)
+
+	return NewSimpleDataList(items)
+}
+
+func (ds *DBSource[T, K]) Pagination(tx *gorm.DB, pageIndex int, pageSize int, dest interface{}, countField ...string) (meta PageMeta, err error) {
+	if pageIndex < 1 {
+		pageIndex = 1
+	}
+	if pageSize < 1 {
+		pageSize = DefaultPageSize
+	}
+	meta.Page = pageIndex
+	meta.PerPage = pageSize
+
+	f := "*"
+	if len(countField) > 0 {
+		f = countField[0]
+	}
+
+	newTx := tx.Session(&gorm.Session{
+		NewDB: true,
+	})
+	err = newTx.Select(fmt.Sprintf("COUNT(%s)", f)).Table("(?) as `aggragate`", tx).Scan(&meta.Total).Error
+	if err != nil {
+		return PageMeta{}, err
+	}
+
+	err = tx.Limit(pageSize).Offset(pageSize * (pageIndex - 1)).Find(dest).Error
+
+	return
+}
+func (ds *DBSource[T, K]) GetItem(key K) (item T, err error) {
+	conds := make([]interface{}, 0)
+	if ds.pk != "" {
+		conds = append(conds, fmt.Sprintf("%s = ?", ds.pk))
+	}
+
+	conds = append(conds, key)
+
+	db := ds.db.Session(&gorm.Session{}).Find(&item, conds...)
+	err = db.Error
+	if db.RowsAffected == 0 {
+		err = gorm.ErrRecordNotFound
+	}
+
+	return
+}
+
+func (ds *DBSource[T, K]) Store(item T) error {
+	return ds.db.Create(&item).Error
+}
+
+func (ds *DBSource[T, K]) Delete(key K) error {
+	var model T
+	conds := make([]interface{}, 0)
+	if ds.pk != "" {
+		conds = append(conds, fmt.Sprintf("%s = ?", ds.pk))
+	}
+
+	conds = append(conds, key)
+
+	return ds.db.Session(&gorm.Session{}).Delete(&model, conds...).Error
+}
+
+func NewDBDataSource[T any, K comparable](db *gorm.DB) *DBSource[T, K] {
+
+	return &DBSource[T, K]{db: db}
+}
+
+func NewDBDataTableSource[T any, K comparable](db *gorm.DB, table string, pk ...string) *DBSource[T, K] {
+	p := "id"
+	if len(pk) > 0 {
+		p = pk[0]
+	}
+	return &DBSource[T, K]{db: db.Table(table), pk: p}
 }
---
+++ source_test.go
@@ -0,0 +1,97 @@
+package datapipe_test
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/enorith/datapipe"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DSN = "root:root@tcp(localhost:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+
+var (
+	source      datapipe.DataSource[User, int64]
+	sourceTable datapipe.DataSource[map[string]interface{}, int64]
+)
+
+type User struct {
+	ID         int64     `gorm:"primaryKey;autoIncrement" json:"id"`
+	UserID     int64     `gorm:"index;not null;comment:user_id" json:"user_id"`
+	Nickname   string    `gorm:"type:varchar(32);not null;default:'';comment:昵称" json:"nickname"`
+	HeadImgUrl string    `gorm:"type:varchar(255);not null;default:'';comment:头像地址" json:"head_img_url"`
+	Sex        int32     `gorm:"not null;default:0;comment:性别（1男；2女）" json:"sex"`
+	Phone      string    `gorm:"type:varchar(32);not null;default:'';comment:手机" json:"phone"`
+	Email      string    `gorm:"type:varchar(125);not null;default:'';comment:邮件" json:"email"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+func (User) TableName() string {
+	return "user"
+}
+
+func TestGetList(t *testing.T) {
+	param := make(datapipe.Params, 0)
+	param.WithPage(2, 5)
+	list := source.GetList(param)
+	j, _ := json.MarshalIndent(list, "", "   ")
+	t.Log("\n", string(j))
+}
+
+func TestGetListTable(t *testing.T) {
+	param := make(datapipe.Params, 0)
+	param.WithPage(2, 5)
+	param.WithScopes(func(db *gorm.DB) *gorm.DB {
+		return db.Select([]string{
+			"id", "user_id", "nickname",
+		})
+	})
+
+	list := sourceTable.GetList(param)
+	t.Log("\n", fmtJson(list))
+}
+
+func TestGetItem(t *testing.T) {
+	item, e := source.GetItem(1)
+	if e != nil {
+		t.Fatal(e)
+	}
+	t.Log("\n", fmtJson(item))
+}
+
+func TestStore(t *testing.T) {
+
+}
+
+func fmtJson(v interface{}) string {
+	j, _ := json.MarshalIndent(v, "", "   ")
+
+	return string(j)
+}
+
+func init() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             time.Second, // 慢 SQL 阈值
+			LogLevel:                  logger.Info, // 日志级别
+			IgnoreRecordNotFoundError: true,        // 忽略ErrRecordNotFound（记录未找到）错误
+		},
+	)
+
+	db, e := gorm.Open(mysql.Open(DSN), &gorm.Config{
+		Logger: newLogger,
+	})
+	if e != nil {
+		log.Fatal(e)
+	}
+	// db.Migrator().AutoMigrate(&User{})
+	source = datapipe.NewDBDataSource[User, int64](db)
+	sourceTable = datapipe.NewDBDataTableSource[map[string]interface{}, int64](db, "user")
+}
